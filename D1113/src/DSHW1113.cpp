d4ff339fbd930477fe598dc87d33dbdd0648fedb
jinganh93 <jingnah93@gmail.com>
2024-11-17 17:42:55 +0800

    second

--- D1113/src/DSHW1113.cpp
+++ D1113/src/DSHW1113.cpp
@@ -3,6 +3,8 @@
 #include <string>
 using namespace std;
 
+
+// 自訂型態二分搜尋
 template <typename T>
 int binaryS(T* arr, int right, int left, T key) {
 	if (right < left) return -1;
@@ -12,16 +14,19 @@ int binaryS(T* arr, int right, int left, T key) {
 	else return mid;
 }
 
+
 template <typename T>
 void inputm(, int n, int q) {
 	T* arr = new T[n];
 	T input;
+	//陣列列讀入
 	for (int i = 0; i < n; i++)
 		cin >> *(arr + i);
 	sort(arr, arr + n - 1);
+	//搜尋值讀入
 	for (int i = 0; i < q; i++) {
 		cin >> input;
-		int result = binaryS(arr, n - 1, 0, input);
+		int result = binaryS(arr, n - 1, 0, input);//搜尋結果列印
 		cout << result << endl;
 	}
 }
@@ -29,7 +34,7 @@ void inputm(, int n, int q) {
 int main(void) {
 	char type;
 	int n, q;
-	while (cin >> type >> n >> q) {
+	while (cin >> type >> n >> q) {//判斷是否有值
 		if (type == 'i') inputm<int>(n, q);
 		if (type == 's') inputm<string>(n, q);
 		cout << endl;
